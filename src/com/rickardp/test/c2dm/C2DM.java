2f3cb5fd559ee4fd819e8752318667930e466980
RickardPettersson <rickardnp@gmail.com>
2011-03-09 09:52:41 +0100

    First commit of Android project to test C2DM

---
+++ src/com/rickardp/test/c2dm/C2DM.java
@@ -0,0 +1,25 @@
+package com.rickardp.test.c2dm;
+
+import com.google.android.c2dm.C2DMessaging;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class C2DM extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		String accountName = "rickard.pettersson@viewserve.com";
+		
+		String regId = C2DMessaging.getRegistrationId(this);
+        if (regId != null && !"".equals(regId)) {
+            Log.i("C2DMTest", "No registration is needed - regId: \"" + regId + "\"");
+        } else {
+            C2DMessaging.register(this, accountName);
+        }
+	}
+}
